eabe69c32f33c933c16bd232e48b130faeaa1a7b
Harryson Palheta <harryson,palheta@gmail.com>
2017-07-21 14:53:49 -0400

    Montagem automática funcionando.

--- js/index.js
+++ js/index.js
@@ -4,39 +4,43 @@ var data;
 
 var vitrine = document.getElementById("vitrine");
 var product = document.querySelectorAll("li");
-console.log(product);
+//console.log(product);
 
 function X(val){
-
+    // elementos
+    var link = product[0].getElementsByClassName('imagem')[0].getElementsByTagName('a')[0];
+    var img = product[0].getElementsByClassName('imagem')[0].getElementsByTagName('a')[0].getElementsByTagName('img')[0];
+    var descricao = product[0].getElementsByClassName('descricao')[0];
+    var preco = product[0].getElementsByClassName('preco')[0];
+    var precoAnterior = product[0].getElementsByClassName('precoAnterior')[0];
+    var pagamento = product[0].getElementsByClassName('pagamento')[0];
     // Recommendation
     val.data.recommendation.forEach(function(e,i){
-//        function createElement(tag, attr){
-//            var node = document.createElement(tag);
-//            var textnode = document.createTextNode(attr);
-//            node.appendChild(textnode);
-//
-//            return node;
-//        }
-//        
-        
+    
         if (!product[i]){
-            var original = product;
-            console.log(i);
-            var node = document.createElement(original);
-            var clone = original.cloneNode(true);
-            vitrine.appendChild(clone);
-            //console.log(original);
+            var li = vitrine.firstElementChild;
+            var cln = li.cloneNode(true);
+            vitrine.appendChild(cln);
+            
         }
     
         // link    
-        product[i].getElementsByClassName('imagem')[0].getElementsByTagName('a')[0].setAttribute("href", "http:" + e.detailUrl);
-        // image
-        product[i].getElementsByClassName('imagem')[0].getElementsByTagName('a')[0].getElementsByTagName('img')[0].setAttribute("src", "http:" + e.imageName);
-        // name
-        product[i].getElementsByClassName('descricao')[0].append(e.name);
-        product[i].getElementsByClassName('preco')[0].append(e.price);
-        product[i].getElementsByClassName('precoVelho')[0].append(e.oldPrice);
-        product[i].getElementsByClassName('pagamento')[0].append(e.productInfo.paymentConditions);
+        link.setAttribute("href", "http:" + e.detailUrl);
+        // imagem
+        img.setAttribute("src", "http:" + e.imageName);
+        // descrição
+        descricao.innerHTML = ""; // limpa
+        descricao.append(e.name); // insere
+        // preço
+        preco.innerHTML = "";   // limpa
+        preco.append(e.price);  // insere
+        // Preço Anterior
+        precoAnterior.innerHTML = ""; // limpa
+        precoAnterior.append(e.oldPrice); // insere
+        // Formas de Pagamento
+        pagamento.innerHTML = ""; //limpa
+        pagamento.insertAdjacentHTML('afterbegin',e.productInfo.paymentConditions); // insere
+
 
     });
     
