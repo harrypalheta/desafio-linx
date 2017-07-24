8221c36f7e8b1ef7f1dab731d61a4ec20295f89d
Harryson Palheta <harryson,palheta@gmail.com>
2017-07-24 13:57:01 -0400

    testes slideshow.

--- js/index.js
+++ js/index.js
@@ -2,8 +2,8 @@
  
 var data;
 
-var vitrine = document.getElementById("vitrine");
-var product = document.querySelectorAll("li");
+var vitrine = document.getElementsByClassName("MultiCarousel");
+var product = document.querySelectorAll(".MultiCarousel-inner");
 //console.log(product);
 
 function X(val){
@@ -16,11 +16,12 @@ function X(val){
     var pagamento = product[0].getElementsByClassName('pagamento')[0];
     // Recommendation
     val.data.recommendation.forEach(function(e,i){
-    
+//    console.log(product);
         if (!product[i]){
-            var li = vitrine.firstElementChild;
-            var cln = li.cloneNode(true);
-            vitrine.appendChild(cln);
+            var item = document.querySelectorAll(".item")[0];
+//            console.log(item);
+            var cln = item.cloneNode(true);
+            vitrine[0].appendChild(cln);
             
         }
     
@@ -47,26 +48,133 @@ function X(val){
 //     console.log(val); 
     //return val;
 }
+//
+//var slideIndex = 1;
+//showDivs(slideIndex);
+//
+//document.getElementById('left').addEventListener("click", function(n){
+//    showDivs(slideIndex += -1);
+//});
+//document.getElementById('right').addEventListener("click", function(n){
+//    showDivs(slideIndex += 1);
+//});
+//function showDivs(n) {
+//  var i;
+//  var x = document.getElementsByTagName("li");
+//  if (n > x.length) {slideIndex = 1}    
+//  if (n < 1) {slideIndex = x.length}
+//  for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";  
+//  }
+//  x[slideIndex-1].style.display = "inline-block";  
+//}
+// 
+//// })();
+//
+$(document).ready(function () {
+    var itemsMainDiv = ('.MultiCarousel');
+    var itemsDiv = ('.MultiCarousel-inner');
+    var itemWidth = "";
 
-var slideIndex = 1;
-showDivs(slideIndex);
+    $('.leftLst, .rightLst').click(function () {
+        var condition = $(this).hasClass("leftLst");
+        if (condition)
+            click(0, this);
+        else
+            click(1, this)
+    });
 
-document.getElementById('left').addEventListener("click", function(n){
-    showDivs(slideIndex += -1);
+    ResCarouselSize();
+
+
+
+
+    $(window).resize(function () {
+        ResCarouselSize();
+    });
+
+    //this function define the size of the items
+    function ResCarouselSize() {
+        var incno = 0;
+        var dataItems = ("data-items");
+        var itemClass = ('.item');
+        var id = 0;
+        var btnParentSb = '';
+        var itemsSplit = '';
+        var sampwidth = $(itemsMainDiv).width();
+        var bodyWidth = $('body').width();
+        $(itemsDiv).each(function () {
+            id = id + 1;
+            var itemNumbers = $(this).find(itemClass).length;
+            btnParentSb = $(this).parent().attr(dataItems);
+            itemsSplit = btnParentSb.split(',');
+            $(this).parent().attr("id", "MultiCarousel" + id);
+
+
+            if (bodyWidth >= 1200) {
+                incno = itemsSplit[3];
+                itemWidth = sampwidth / incno;
+            }
+            else if (bodyWidth >= 992) {
+                incno = itemsSplit[2];
+                itemWidth = sampwidth / incno;
+            }
+            else if (bodyWidth >= 768) {
+                incno = itemsSplit[1];
+                itemWidth = sampwidth / incno;
+            }
+            else {
+                incno = itemsSplit[0];
+                itemWidth = sampwidth / incno;
+            }
+            $(this).css({ 'transform': 'translateX(0px)', 'width': itemWidth * itemNumbers });
+            $(this).find(itemClass).each(function () {
+                $(this).outerWidth(itemWidth);
+            });
+
+            $(".leftLst").addClass("over");
+            $(".rightLst").removeClass("over");
+
+        });
+    }
+
+
+    //this function used to move the items
+    function ResCarousel(e, el, s) {
+        var leftBtn = ('.leftLst');
+        var rightBtn = ('.rightLst');
+        var translateXval = '';
+        var divStyle = $(el + ' ' + itemsDiv).css('transform');
+        console.log(divStyle);
+        var values = divStyle.match(/-?[\d\.]+/g);
+        var xds = Math.abs(values[4]);
+        if (e == 0) {
+            translateXval = parseInt(xds) - parseInt(itemWidth * s);
+            $(el + ' ' + rightBtn).removeClass("over");
+
+            if (translateXval <= itemWidth / 2) {
+                translateXval = 0;
+                $(el + ' ' + leftBtn).addClass("over");
+            }
+        }
+        else if (e == 1) {
+            var itemsCondition = $(el).find(itemsDiv).width() - $(el).width();
+            translateXval = parseInt(xds) + parseInt(itemWidth * s);
+            $(el + ' ' + leftBtn).removeClass("over");
+
+            if (translateXval >= itemsCondition - itemWidth / 2) {
+                translateXval = itemsCondition;
+                $(el + ' ' + rightBtn).addClass("over");
+            }
+        }
+        $(el + ' ' + itemsDiv).css('transform', 'translateX(' + -translateXval + 'px)');
+    }
+
+    //It is used to get some elements from btn
+    function click(ell, ee) {
+        var Parent = "#" + $(ee).parent().attr("id");
+        var slide = $(Parent).attr("data-slide");
+        ResCarousel(ell, Parent, slide);
+    }
+
 });
-document.getElementById('right').addEventListener("click", function(n){
-    showDivs(slideIndex += 1);
-});
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByTagName("li");
-  if (n > x.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "block";  
-  }
-  x[slideIndex-1].style.display = "block";  
-}
- 
-// })();
-
